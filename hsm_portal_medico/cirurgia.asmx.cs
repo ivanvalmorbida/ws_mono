2987e97b733b2af3a526c657d68cb9e73d601a79
ivanvalmorbida <ivanvalmorbida@gmail.com>
2018-06-01 09:13:35 -0300

    ivan

---
+++ hsm_portal_medico/cirurgia.asmx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections;
+using Newtonsoft.Json;
+
+namespace ws_mono
+{
+	[WebService(Namespace = "http://www.example.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    public class cirurgia : System.Web.Services.WebService
+    {
+		[WebMethod]
+		public string getCirurgiaCod(string strCod)
+		{
+			Conexao cn = new Conexao();
+            SqlParameter sqlPar = new SqlParameter();
+            ArrayList colPar = new ArrayList();
+            StringBuilder strSQL = new StringBuilder();
+            DataTable tb;
+
+            sqlPar.DbType = DbType.String;
+            sqlPar.Value = strCod;
+            sqlPar.ParameterName = "@Cod";
+            colPar.Add(sqlPar);
+
+			strSQL.Append("SELECT Codigo, Nome FROM Exame where Codigo=@Cod").AppendLine();
+
+            tb = cn.OpenDataSetWithParam(strSQL.ToString(), "Cirurgia", colPar).Tables[0];
+
+            return JsonConvert.SerializeObject(tb, Formatting.None);			
+		}
+    }
+}
---
+++ hsm_portal_medico/js/cirurgia.js
@@ -0,0 +1,31 @@
+﻿angular.module('CirurgiasApp', ['ngMaterial','angularSoap'])
+.factory("CirurgiaService", ['$soap',function($soap){
+    var base_url = "http://127.0.0.1:8080/cirurgia.asmx"
+
+    var getCirurgiaCod = function(cod){
+            return $soap.post(base_url,"getCirurgiaCod", {strCod: cod})
+        }
+
+    return {
+        getCirurgiaCod:getCirurgiaCod
+    }
+}])
+.controller('CirurgiasCtrl', function($http, $scope, CirurgiaService) {
+    $scope.procedimentos = []
+
+    $scope.addProc = function() {
+        $scope.procedimentos.push({
+            'procedimento': $scope.procedimento, 'descricao': $scope.descricao
+        })
+        $scope.procedimento = null
+        $scope.descricao = null
+    }
+
+    $scope.BuscarCodigo = function() {
+        CirurgiaService.getCirurgiaCod($scope.procedimento).then(function(response){
+            pro = JSON.parse(response)
+            if(pro.length!=0) 
+                $scope.descricao = pro[0].Nome
+        })
+    }
+})
